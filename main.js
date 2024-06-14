7af1df24733664473e479eb2f32c511c42781c1c
ayumi222 <108939482+ayumi222@users.noreply.github.com>
2024-06-14 08:57:37 -0300

    14/06

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
+contador[0].textContent ="OLÁ"
+const tempoObjetivo1 = new Date (2024,7,6,21,30)
+const tempoObjetivo2 = new Date (2024,3,10,5,33)
+const tempoObjetivo3 = new Date (2024,7,16,10,50)
+const tempoObjetivo4 = new Date (2024,11,01,11,28)
 
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
@@ -12,4 +17,3 @@ for(let i = 0 ; i < botoes.length; i++){
      conteudo[i].classList.add("ativo")
     }
 }
-
