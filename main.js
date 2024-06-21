88f2bccb3d1192edb6c0228fe817655b631dd107
ayumi222 <108939482+ayumi222@users.noreply.github.com>
2024-06-21 08:58:13 -0300

    21/06

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const contador = document.querySelectorAll(".contador");
 
 
 const tempoObjetivo1 = new Date (2024,7,6,21,30)
-const tempoObjetivo2 = new Date (2024,3,10,5,33)
+const tempoObjetivo2 = new Date (2024,9,10,5,33)
 const tempoObjetivo3 = new Date (2024,7,16,10,50)
 const tempoObjetivo4 = new Date (2024,11,01,11,28)
 let agora = new Date();
@@ -13,6 +13,15 @@ contador[0].textContent = tempoObjetivo1-agora
 contador[1].textContent = tempoObjetivo2-agora
 contador[2].textContent = tempoObjetivo3-agora
 contador[3].textContent = tempoObjetivo4-agora
+let segundos
+let minutos
+let horas 
+let dias 
+segundos = (tempoObjetivo1-agora)/1000;
+minutos = (tempoObjetivo1-agora)/60;
+horas = (tempoObjetivo1-agora)/60;
+dias = (tempoObjetivo1-agora)/24;
+contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
