ffef71b2b3b894b4d22fd9fd4573f37ecc8ab90b
Karoliny Nascimento <108940073+KKojiio@users.noreply.github.com>
2024-08-08 09:45:16 -0300

    08/08/2024

--- main.js
+++ main.js
@@ -29,7 +29,8 @@ minutos = minutos%60
 horas = horas%24
 return[dias, horas, minutos, segundos]
 }
-let contadorPrimeiro = calculaTempo(tempoObjetivo1)
+
+/*let contadorPrimeiro = calculaTempo(tempoObjetivo1)
 contador[0].textContent = `faltam ${contadorPrimeiro[0]} dias, ${contadorPrimeiro[1]} horas, ${contadorPrimeiro[2]} minutos, ${contadorPrimeiro[3]} segundos`
 let contadorSegundo = calculaTempo(tempoObjetivo2)
 contador[1].textContent = `faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas, ${contadorSegundo[2]} minutos, ${contadorSegundo[3]} segundos`
@@ -37,7 +38,7 @@ let contadorTerceiro = calculaTempo(tempoObjetivo3)
 contador[2].textContent = `faltam ${contadorTerceiro[0]} dias, ${contadorTerceiro[1]} horas, ${contadorTerceiro[2]} minutos, ${contadorTerceiro[3]} segundos`
 let contadorQuarto = calculaTempo(tempoObjetivo4)
 contador[3].textContent = `faltam ${contadorQuarto[0]} dias, ${contadorQuarto[1]} horas, ${contadorQuarto[2]} minutos, ${contadorQuarto[3]} segundos`
-
+*/
 
 
 for(let i = 0 ; i < botoes.length; i++){
